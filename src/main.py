cd9940708f9aaaadda1f3a37069ff93b2b1c93c7
saum7800 <gandhisaumya8@gmail.com>
2020-03-23 17:42:15 +0530

    added start stop and erasing

--- src/main.py
+++ src/main.py
@@ -1,14 +1,24 @@
 import numpy as np
 import cv2
-#79 200 30
+
+
 redLower = np.array([0, 0, 120])
 redUpper = np.array([90, 90, 255])
+
 redLowerHSV = np.array([0, 150, 150])
 redUpperHSV = np.array([30, 255, 255])
+
 final_img = np.zeros((480, 640, 3))+255
+
 kernel = np.ones((5, 5), np.uint8)
+
+drawing = False
+erasing = False
+
 prev_cx = None
 prev_cy = None
+
+win_name = input("name of image:")
 
 cap = cv2.VideoCapture(0)
 
@@ -23,23 +33,33 @@ while cap.isOpened():
     cv2.imshow('win', red_extract)
     # Find contours in the image
     (cnts, _) = cv2.findContours(red_extract.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    if len(cnts) > 0:
+    if len(cnts) > 0 and drawing is True:
         cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
         M = cv2.moments(cnts[0])
         cx = int(M['m10'] / M['m00'])
         cy = int(M['m01'] / M['m00'])
         if prev_cx is not None:
-            cv2.line(final_img, (prev_cx, prev_cy), (cx, cy), (0, 0, 0), 2)
+            if abs(cx - prev_cx) <= 30 and abs(cy - prev_cy) <= 30:
+                if erasing is True:
+                    cv2.circle(final_img, (cx,cy), 10, (255, 255, 255), -1)
+                else:
+                    cv2.line(final_img, (prev_cx, prev_cy), (cx, cy), (0, 0, 0), 2)
         prev_cx = cx
         prev_cy = cy
-
-        #cv2.circle(final_img, (cx, cy), 10, (0, 0, 255), -1)
+    else:
+        prev_cx = None
+        prev_cy = None
 
     img = cv2.drawContours(frame, cnts, -1, (0, 255, 0), 3)
     cv2.imshow('win2', img)
-    cv2.imshow('drawWin', final_img)
+    cv2.imshow(win_name, final_img)
     k = cv2.waitKey(1) & 0xff
     if k == ord('q'):
         break
+    elif k == ord('s'):
+        drawing = not drawing
+    elif k == ord('e'):
+        erasing = not erasing
+
 cap.release()
 cv2.destroyAllWindows()
